c2a2234b63f71ecb1f53e259fb9e874dc66cbf47
Sylvain CARTON <sylvain@babaoo.com>
2022-11-18 10:30:42 +0100

    adding examples

--- Header.php
+++ Header.php
@@ -5,10 +5,10 @@ include 'Dao/DishDao.php';
 include 'Models/Dish.php';
 include 'Constants/Startup.php';
 
-    // $DishDao = new DishDao();
+     $DishDao = new DishDao();
     echo "Connected successfully\n";
 
-    // $Dish = new Dish(2, 0, "Kebab", 20, DishType::Main);
-    // print_r($Dish->Name);
-    // print_r($DishDao->Update(2, 0, "Kefta", 20, DishType::Main));
+    $Dish = new Dish(2, 0, "Kebab", 20, DishType::Main);
+    print_r($Dish->Name);
+    print_r($DishDao->Update(2, 0, "Kefta", 20, DishType::Main));
 ?>
